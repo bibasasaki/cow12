af2477a8840920ed535c82fc2377eb0415fab526
bibasasaki <160513437+bibasasaki@users.noreply.github.com>
2024-12-18 13:56:21 +0300

    stick animation and script

---
+++ shooter1/Assets/Scriplts/stickattack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickattack : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            anim.SetBool("attacking",true);
+        else if(Input.GetButtonUp("Fire1"))
+        
+           
+    }
+}
